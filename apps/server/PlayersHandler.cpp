eaa9e2cd5f5dd138c59b27948e22ebd5e3eff8fa
Pokawa <cwajdahubert@gmail.com>
2020-12-27 11:25:21 +0100

    PlayersHandler

---
+++ apps/server/PlayersHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hubert on 27.12.2020.
+//
+
+#include "PlayersHandler.hpp"
---
+++ apps/server/PlayersHandler.hpp
@@ -0,0 +1,14 @@
+//
+// Created by hubert on 27.12.2020.
+//
+
+#ifndef CHARADES_PLAYERSHANDLER_HPP
+#define CHARADES_PLAYERSHANDLER_HPP
+
+
+class PlayersHandler {
+
+};
+
+
+#endif //CHARADES_PLAYERSHANDLER_HPP
