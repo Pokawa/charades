4e2fe27fc491cf096c563d425e622dac9c4dfde3
Pokawa <cwajdahubert@gmail.com>
2020-12-29 17:47:02 +0100

    IO queues in server

---
+++ apps/server/IoHandler.cpp
@@ -0,0 +1,33 @@
+//
+// Created by hubert on 29.12.2020.
+//
+
+#include "IoHandler.hpp"
+
+void IOHandler::addClient(const chs::WebSocket &socket) {
+    incomingQueues.emplace(socket.getDescriptor(), socket);
+    outgoingQueues.emplace(socket.getDescriptor(), socket);
+}
+
+void IOHandler::removeClient(const chs::WebSocket &socket) {
+    incomingQueues.erase(socket.getDescriptor());
+    outgoingQueues.erase(socket.getDescriptor());
+}
+
+void IOHandler::receiveFrom(const chs::WebSocket &socket) {
+    incomingQueues.at(socket.getDescriptor()).readMessages();
+}
+
+void IOHandler::sendTo(const chs::WebSocket &socket) {
+    outgoingQueues.at(socket.getDescriptor()).sendMessages();
+}
+
+void IOHandler::putMessage(const chs::WebSocket &socket, const chs::Message & message) {
+    outgoingQueues.at(socket.getDescriptor()).putMessage(message);
+}
+
+chs::Message IOHandler::getMessage(const chs::WebSocket &socket) {
+    return incomingQueues.at(socket.getDescriptor()).getMessage();
+}
+
+
---
+++ apps/server/IoHandler.hpp
@@ -0,0 +1,25 @@
+//
+// Created by hubert on 29.12.2020.
+//
+
+#pragma once
+
+#include <map>
+#include <WebSocket.hpp>
+#include <IncomingMessageQueue.hpp>
+#include <OutgoingMessageQueue.hpp>
+
+class IOHandler {
+private:
+    std::map<int, chs::IncomingMessageQueue> incomingQueues;
+    std::map<int, chs::OutgoingMessageQueue> outgoingQueues;
+
+public:
+
+    void addClient(const chs::WebSocket & socket);
+    void removeClient(const chs::WebSocket & socket);
+    void receiveFrom(const chs::WebSocket & socket);
+    void sendTo(const chs::WebSocket & socket);
+    void putMessage(const chs::WebSocket & socket, const chs::Message & message);
+    chs::Message getMessage(const chs::WebSocket & socket);
+};
